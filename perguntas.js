77184b8b51753f723d01393209ab613abe10d1aa
lauralejaricob <160238139+lauralejaricob@users.noreply.github.com>
2024-10-07 07:41:46 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,10 +1,4 @@
 criaCartao (
-    'Inglês',
-    'O que a palavra "legend" significa em português?',
-    'Legenda'
-)
-
-criaCartao (
     'Medicina',
     'Normalmente, quantos litros de sangue uma pessoa tem?',
     'Tem entre 4 a 6 litros'
@@ -14,4 +8,28 @@ criaCartao (
     'Matemática',
     'Quantas casas decimais tem o número pi?',
     'Infinitas'
+)
+
+criaCartao (
+    'Quimica',
+    'Em que ordem surgiram os modelos atômicos?',
+    'Dalton, Thomson, Rutherford, Rutherford-Bohr.'
+)
+
+criaCartao (
+    'Historia',
+    'Em que período da pré-história o fogo foi descoberto?',
+    'Paleolítico'
+)
+
+criaCartao (
+    'Literatura',
+    'Quem é o autor de “O Príncipe”?',
+    'Maquiavel'
+)
+
+criaCartao (
+    'Geografia',
+    'Qual destes países é transcontinental?',
+    'Rússia'
 )
